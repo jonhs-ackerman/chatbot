a8c7fc8b3a92c8168cd03016edd7cb13b28485ce
Destin-blaster <dancileciribagula@gmail.com>
2024-11-13 22:59:11 +0200

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -1,20 +1,22 @@
 module.exports = {
-	config: {
-			name: "prefix",
-			version: "1.0",
-			author: "Shizuka junior",
-			countDown: 5,
-			role: 0,
-			shortDescription: "prefix bot",
-			longDescription: "le prefix du bot",
-			category: "prefix",
-	},
-onStart: async function(){}, 
-onChat: async function({
-	event,
-	message,
-	getLang
-}) {
-	if (event.body && event.body.toLowerCase() == "prefix") return message.reply("---------------- » «» « ----------------\n mon prefix est 🩸+🩸---------------- » «» « ----------------\n si tu as un probleme avec une commande tape +help(nom de la commande) pour plus des details💁‍♂️");
+ config: {
+	 name: "prefix",
+	 version: "1.0",
+	 author: "Tokodori_Frtiz",//remodified by cliff
+	 countDown: 5,
+	 role: 0,
+	 shortDescription: "no prefix",
+	 longDescription: "no prefix",
+	 category: " ",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "prefix") {
+ return message.reply({
+ body: `\n┏━━━━━━┓\n𝑷𝑹𝑬𝑭𝑰𝑿 ↠# \n/┗━━━━━━┛`,
+ attachment: await global.utils.getStreamFromURL("https://i.ibb.co/JHgwRHD/image.jpg")
+ });
+ }
+ }
 }
-};
